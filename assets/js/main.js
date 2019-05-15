8ae276e7c42ff1d70e6a080bfcfbfc86c2ba7ddd
Bas Tichelaar <bas@tichelaar.io>
2019-05-15 08:31:08 +0200

    Added underline for links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
 
+$(function() {
+  var $el,
+    leftPos,
+    newWidth,
+    $mainNav = $(".navbar-nav");
+
+  $mainNav.append("<li id='magic-line'></li>");
+  var $magicLine = $("#magic-line");
+
+  $magicLine
+    .width($(".active").width())
+    .css("left", $(".active a").position().left)
+    .data("origLeft", $magicLine.position().left)
+    .data("origWidth", $magicLine.width());
+
+  $(".navbar-nav li a").hover(
+    function() {
+      $el = $(this);
+      leftPos = $el.position().left;
+      newWidth = $el.parent().width();
+      $magicLine.stop().animate({
+        left: leftPos,
+        width: newWidth
+      });
+    },
+    function() {
+      $magicLine.stop().animate({
+        left: $magicLine.data("origLeft"),
+        width: $magicLine.data("origWidth")
+      });
+    }
+  );
+});
+
+
   $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar");
@@ -7,6 +42,9 @@ $(document).ready(function () {
 
       var $navbarbrand = $(".navbar-brand");
       $navbarbrand.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+
+      var $magicline = $("#magic-line");
+      $magicline.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
   });
 
